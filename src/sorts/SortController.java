223ae26a780494329e1abffb68eab6dd32e2c8ff
Kurrry <ajohnsond11@gmail.com>
2023-05-31 11:26:14 -0600

    Implemented and tested both versions of QuickSort. Refactored code that might not have been jdk 8 compatible, despite my home pc running on jdk 8 and being able to run it. If my code was not functional before I apologize.

--- src/sorts/SortController.java
+++ src/sorts/SortController.java
@@ -5,23 +5,23 @@ import java.util.Comparator;
 public final class SortController {
     public static <T> void sort (Comparable<T>[] shapes, char sortType) {
         switch (sortType) {
-            case 'b' -> BubbleSort.doBubbleSort(shapes);
-            case 's' -> SelectionSort.doSelectionSort(shapes);
-            case 'i' -> InsertionSort.doInsertionSort(shapes);
-            case 'm' -> MergeSort.doMergeSort(shapes);
-            //case 'q' -> new QuickSort(shapes);
-            //case 'z' -> new RadixSort(shapes);
+            case 'b': BubbleSort.doBubbleSort(shapes); break;
+            case 's': SelectionSort.doSelectionSort(shapes); break;
+            case 'i': InsertionSort.doInsertionSort(shapes); break;
+            case 'm': MergeSort.doMergeSort(shapes); break;
+            case 'q': QuickSort.doQuickSortNoCT(shapes); break;
+            //case 'z': new RadixSort(shapes); break;
         }
     }
 
     public static <T> void sort (Comparable<T>[] shapes, char sortType, Comparator<T> compareType) {
         switch (sortType) {
-            case 'b' -> BubbleSort.doBubbleSort(shapes, compareType);
-            case 's' -> SelectionSort.doSelectionSort(shapes, compareType);
-            case 'i' -> InsertionSort.doInsertionSort(shapes, compareType);
-            case 'm' -> MergeSort.doMergeSort(shapes, compareType);
-            //case 'q' -> new QuickSort(shapes, compareType);
-            //case 'z' -> new RadixSort(shapes, compareType);
+            case 'b': BubbleSort.doBubbleSort(shapes, compareType); break;
+            case 's': SelectionSort.doSelectionSort(shapes, compareType); break;
+            case 'i': InsertionSort.doInsertionSort(shapes, compareType); break;
+            case 'm': MergeSort.doMergeSort(shapes, compareType); break;
+            case 'q': QuickSort.doQuickSortCT(shapes, compareType); break;
+            //case 'z': new RadixSort(shapes, compareType); break;
         }
     }
 }
