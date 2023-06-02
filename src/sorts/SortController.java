248cfbac6090749d1fb14d9a7c372a81d44cd949
Kurrry <ajohnsond11@gmail.com>
2023-06-02 11:18:47 -0600

    Fixed sorts to output in descending order, according to assignment specs.

--- src/sorts/SortController.java
+++ src/sorts/SortController.java
@@ -10,7 +10,7 @@ public final class SortController {
             case 'i': InsertionSort.doInsertionSort(shapes); break;
             case 'm': MergeSort.doMergeSort(shapes); break;
             case 'q': QuickSort.doQuickSortNoCT(shapes); break;
-            case 'z': HeapSort.doHeapSort(shapes); break;
+            case 'z': //HeapSort.doHeapSort(shapes); break;
         }
     }
 
@@ -21,7 +21,7 @@ public final class SortController {
             case 'i': InsertionSort.doInsertionSort(shapes, compareType); break;
             case 'm': MergeSort.doMergeSort(shapes, compareType); break;
             case 'q': QuickSort.doQuickSortCT(shapes, compareType); break;
-            case 'z': HeapSort.doHeapSort(shapes, compareType); break;
+            case 'z': //HeapSort.doHeapSort(shapes, compareType); break;
         }
     }
 }
